4fa616a4945df596e557e4c47d56f8a221abdd28
Jolo Cansana <32953366+jolocansana@users.noreply.github.com>
2021-09-11 13:40:48 +0800

    tasklist + timer + gameover

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,24 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLogic : MonoBehaviour
 {
+    public GameObject taskListCanvas;
+    public GameObject timerCanvas;
+    public GameObject pauseMenu;
+    public GameObject playerCanvas;
 
     public GameObject sampleMinigameCanvas;
+
+    private List<string> taskList;
+
+    private float timeLeft = 5; // in seconds, default 5 minutes
+    public bool isGamePaused = false;
+    public bool isGameDone = false;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        EventBroadcaster.Instance.AddObserver(EventNames.GameJam.COMPLETE_TASK, this.completeTask);
-        EventBroadcaster.Instance.AddObserver(EventNames.GameJam.START_MINIGAME, this.startMinigame);
+        EventBroadcaster.Instance.AddObserver(EventNames.GameJam.COMPLETE_TASK, this.CompleteTask);
+        EventBroadcaster.Instance.AddObserver(EventNames.GameJam.START_MINIGAME, this.StartMinigame);
+
+        taskList = new List<string>();
+        taskList.Add("SampleMinigame");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateTasklist();
+        PauseHandler();
+
+        if (!isGamePaused && !isGameDone)
+        {
+            if (timeLeft > 0)
+            {
+                float minutes = Mathf.FloorToInt(timeLeft / 60);
+                float seconds = Mathf.FloorToInt(timeLeft % 60);
+                timerCanvas.GetComponent<Text>().text = string.Format("{0:00}:{1:00}", minutes, seconds);
+                timeLeft -= Time.deltaTime;
+            }
+            else // GAME OVER CONDITIONS
+            {
+                GameOver(false);
+            }
+        }
     }
 
     private void OnDestroy()
@@ -27,10 +58,54 @@ public class GameLogic : MonoBehaviour
         EventBroadcaster.Instance.RemoveObserver(EventNames.GameJam.START_MINIGAME);
     }
 
-    void startMinigame(Parameters param)
+    void PauseHandler()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Parameters param = new Parameters();
+            if (isGamePaused) // if game is paused
+            {
+                pauseMenu.SetActive(false);
+                playerCanvas.SetActive(true);
+                isGamePaused = false;
+                param.PutExtra(EventNames.Param.TOGGLE_CHARACTER, !isGamePaused);
+                EventBroadcaster.Instance.PostEvent(EventNames.Param.TOGGLE_CHARACTER, param);
+            }
+            else // if game is not paused
+            {
+                pauseMenu.SetActive(true);
+                playerCanvas.SetActive(false);
+                isGamePaused = true;
+                param.PutExtra(EventNames.Param.TOGGLE_CHARACTER, !isGamePaused);
+                EventBroadcaster.Instance.PostEvent(EventNames.Param.TOGGLE_CHARACTER, param);
+            }
+        }
+    }
+
+    void UpdateTasklist()
+    {
+        string taskString = "";
+
+        foreach(string task in taskList)
+        {
+            taskString = taskString + "- " + task + "\n"; 
+        }
+        
+        taskListCanvas.GetComponent<Text>().text = taskString;
+    }
+
+    void GameOver(bool isSuccess) // if true, then win; if false, then lose
+    {
+        Debug.Log("GG KA BOI");
+        isGameDone = true;
+    }
+
+    void StartMinigame(Parameters param)
     {
         string taskName = param.GetStringExtra(EventNames.Param.TASK_NAME, "defaultValue");
         Debug.Log(taskName);
+
+        playerCanvas.SetActive(false);
 
         param.PutExtra(EventNames.Param.TOGGLE_CHARACTER, false);
         EventBroadcaster.Instance.PostEvent(EventNames.Param.TOGGLE_CHARACTER, param);
@@ -46,9 +121,11 @@ public class GameLogic : MonoBehaviour
         }
     }
 
-    void completeTask(Parameters param)
+    void CompleteTask(Parameters param)
     {
         string taskName = param.GetStringExtra(EventNames.Param.TASK_NAME, "defaultValue");
+
+        playerCanvas.SetActive(true);
 
         param.PutExtra(EventNames.Param.TOGGLE_CHARACTER, true);
         EventBroadcaster.Instance.PostEvent(EventNames.Param.TOGGLE_CHARACTER, param);
--- Assets/Scripts/PauseMenuLogic.cs
+++ Assets/Scripts/PauseMenuLogic.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class PauseMenuLogic : MonoBehaviour
 {
-    public bool isGamePaused = false;
 
-    public GameObject pauseMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -17,23 +15,6 @@ public class PauseMenuLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("escape"))
-        {
-            Parameters param = new Parameters();
-            if (isGamePaused) // if game is paused
-            {
-                pauseMenu.SetActive(false);
-                isGamePaused = false;
-                param.PutExtra(EventNames.Param.TOGGLE_CHARACTER, !isGamePaused);
-                EventBroadcaster.Instance.PostEvent(EventNames.Param.TOGGLE_CHARACTER, param);
-            }
-            else // if game is not paused
-            {
-                pauseMenu.SetActive(true);
-                isGamePaused = true;
-                param.PutExtra(EventNames.Param.TOGGLE_CHARACTER, !isGamePaused);
-                EventBroadcaster.Instance.PostEvent(EventNames.Param.TOGGLE_CHARACTER, param);
-            }
-        }
+
     }
 }
